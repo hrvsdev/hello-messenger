650d2bdd9b323db87b5ef5bcf8d016ce63b7bc75
kingvyas <kumarvyasss@gmail.com>
2022-07-26 11:19:57 +0530

    added sass dependencies

---
+++ components/login/Login.tsx
@@ -0,0 +1,5 @@
+import "./Login.scss";
+
+export default function Login(): JSX.Element {
+  return <div>Login</div>;
+}
---
+++ pages/login/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import Login from "../../components/login";
+
+const LoginPage: NextPage = (): JSX.Element => {
+  return <Login />;
+};
+
+export default LoginPage
