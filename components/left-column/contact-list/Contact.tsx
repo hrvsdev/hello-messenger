882d687982477af86014d909d6f0e724be2b3eff
kingvyas <kumarvyasss@gmail.com>
2022-08-02 20:14:11 +0530

    adding hookstate state instead of use state

--- components/left-column/contact-list/Contact.tsx
+++ components/left-column/contact-list/Contact.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React  from "react";
+import { useState } from "@hookstate/core";
 import { useMenuState } from "@szhsin/react-menu";
 import styled from "styled-components";
 import Image from "next/image";
