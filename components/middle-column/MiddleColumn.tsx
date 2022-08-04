49db1ac176663517a3fd28120b677e06007b5c9e
kingvyas <kumarvyasss@gmail.com>
2022-08-04 16:02:13 +0530

    added header avatar in middle section

--- components/middle-column/MiddleColumn.tsx
+++ components/middle-column/MiddleColumn.tsx
@@ -1,14 +1,19 @@
 import styled from "styled-components";
+import Footer from "./footer";
 import Header from "./header";
 
 export default function MiddleColumn(): JSX.Element {
   return (
     <MiddleColWrapper>
       <Header />
+      <Footer />
     </MiddleColWrapper>
   );
 }
 
 const MiddleColWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   flex-grow: 1;
 `;
--- components/middle-column/header/Header.tsx
+++ components/middle-column/header/Header.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import Image from "next/image";
+
 import { BiDotsHorizontalRounded } from "react-icons/bi";
 
 export default function Header(): JSX.Element {
@@ -6,7 +8,9 @@ export default function Header(): JSX.Element {
     <HeaderWrapper>
       <Left>
         <UserInfo>
-          <Picture></Picture>
+          <Picture>
+          <Image src="https://i.pravatar.cc/750" alt="An image" width="40" height="40" />
+          </Picture>
           <Info>
             <Name>User name</Name>
             <Status>last seen recently</Status>
@@ -41,11 +45,14 @@ const UserInfo = styled.div`
 
 const Picture = styled.div`
   display: flex;
-  background: #ccc;
   width: 40px;
   height: 40px;
   margin-right: 15px;
   border-radius: 50%;
+
+  img{
+    border-radius: 20px;
+  }
 `;
 
 const Info = styled.div`
