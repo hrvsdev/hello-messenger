8c33e67e001d297ce8b1419fa403bff69e1ba5eb
kingvyas <kumarvyasss@gmail.com>
2022-08-04 16:06:50 +0530

    adding footer

--- components/middle-column/footer/Footer.tsx
+++ components/middle-column/footer/Footer.tsx
@@ -1,3 +1,10 @@
+import styled from "styled-components";
+
 export default function Footer(): JSX.Element {
   return <div>Footer</div>;
 }
+
+
+const FooterWrapper = styled.div`
+    height: 60px;
+`
