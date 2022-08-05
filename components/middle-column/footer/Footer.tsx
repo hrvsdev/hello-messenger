73df0eb69c09695aee509bda3861037fc464eb39
kingvyas <kumarvyasss@gmail.com>
2022-08-05 08:10:40 +0530

    added input and srtyles to footer

--- components/middle-column/footer/Footer.tsx
+++ components/middle-column/footer/Footer.tsx
@@ -1,9 +1,23 @@
 import styled from "styled-components";
 
+import { BiSmile, BiImageAlt } from "react-icons/bi";
+
 export default function Footer(): JSX.Element {
   return (
     <FooterWrapper>
-      <Left></Left>
+      <Left>
+        <InputRootWrapper>
+          <IconWrapper>
+            <BiSmile />
+          </IconWrapper>
+          <InputWrapper>
+            <input type="text" />
+          </InputWrapper>
+          <IconWrapper>
+            <BiImageAlt />
+          </IconWrapper>
+        </InputRootWrapper>
+      </Left>
       <Right></Right>
     </FooterWrapper>
   );
@@ -23,10 +37,38 @@ const Left = styled.div`
   align-items: center;
   flex-grow: 1;
 `;
+
+const InputRootWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  background: #ccc;
+  border-radius: 27px;
+  height: 54px;
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  width: 38px;
+  height: 38px;
+
+  svg {
+    width: 22px;
+    height: 22px;
+  }
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
+`;
+
 const Right = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 `;
-
-const 
