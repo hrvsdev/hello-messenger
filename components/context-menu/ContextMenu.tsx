3fdd7d914cc7941c6f6d8934d1bab24210b229a0
kingvyas <kumarvyasss@gmail.com>
2022-08-08 14:19:22 +0530

    creating custom context menu

--- components/context-menu/ContextMenu.tsx
+++ components/context-menu/ContextMenu.tsx
@@ -1,5 +1,27 @@
-export default function ContextMenu(): JSX.Element {
-  return (
-    <div>ContextMenu</div>
-  )
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { StateMethods } from "@hookstate/core";
+
+interface props {
+  showMenu: StateMethods<boolean>;
+  points: { x: number; y: number };
 }
+
+export default function ContextMenu({ showMenu, points }: props): JSX.Element {
+  return showMenu.value ? (
+    <MenuWrapper style={{ x: points.x, y: points.y }}>ContextMenu</MenuWrapper>
+  ) : (
+    <div></div>
+  );
+}
+
+const MenuWrapper = styled(motion.div)`
+  position: absolute;
+  z-index: 1;
+  background: rgba(255, 255, 255, 0.733);
+  backdrop-filter: blur(10px);
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  padding: 8px;
+  border-radius: 12px;
+  width: 200px;
+`;
--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,20 +1,35 @@
+import { useState } from "@hookstate/core";
 import styled from "styled-components";
 
 import LeftColumn from "../left-column";
 import MiddleColumn from "../middle-column";
 import RightColumn from "../right-column";
+import ContextMenu from "../context-menu";
 
-export default function home(): JSX.Element {
+export default function Home(): JSX.Element {
+  const showMenu = useState(false);
+  const points = useState({x: 0, y: 0})
+
+  const onContextMenu = (e: React.MouseEvent) => {
+    const {clientWidth, clientHeight} = e.currentTarget
+    const {clientX, clientY} = e
+    e.preventDefault();
+    console.log({clientWidth, clientHeight})
+    console.log({clientX, clientY})
+    points.set({x: e.clientX, y: e.clientY})
+    showMenu.set((prev) => !prev);
+  };
+
   return (
-    <Main>
+    <Main onContextMenu={onContextMenu}>
       <LeftColumn />
-      <MiddleColumn/>
-      <RightColumn/>
+      <MiddleColumn />
+      <RightColumn />
+      <ContextMenu showMenu={showMenu} points={points.value}/>
     </Main>
   );
 }
 
 const Main = styled.main`
   display: flex;
-  
 `;
