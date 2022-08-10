38dfe21b3586ed61b14c5c9ba032a80d965f8528
kingvyas <kumarvyasss@gmail.com>
2022-08-10 08:46:57 +0530

    removed custom menu

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,28 +1,15 @@
-import { useState } from "@hookstate/core";
 import styled from "styled-components";
 
 import LeftColumn from "../left-column";
 import MiddleColumn from "../middle-column";
 import RightColumn from "../right-column";
-import ContextMenu from "../context-menu";
-import getMenuXandY from "../context-menu/utils/getMenuXandY";
 
 export default function Home(): JSX.Element {
-  const menuState = useState<boolean>(false);
-  const XandY = useState({ points: { x: 0, y: 0 }, origins: { originX: 0, originY: 0 } });
-
-  const onContextMenu = (e: React.MouseEvent) => {
-    menuState.set(false);
-    XandY.set(getMenuXandY(e, 200, 400));
-    setTimeout(() => menuState.set(true), 100);
-  };
-
   return (
-    <Main onContextMenu={onContextMenu}>
+    <Main>
       <LeftColumn />
       <MiddleColumn />
       <RightColumn />
-      <ContextMenu menuState={menuState} XandY={XandY.get()} />
     </Main>
   );
 }
