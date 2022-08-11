43dc4c3d3ecf3a9de737917e4d3e4f5a8457cc74
kingvyas <kumarvyasss@gmail.com>
2022-08-11 14:29:46 +0530

    added firebase config

---
+++ firebase/config.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBu_mPmwrTzkRDSUDeF1-R-43JRChmhBo4",
+  authDomain: "hello-msgr.firebaseapp.com",
+  projectId: "hello-msgr",
+  storageBucket: "hello-msgr.appspot.com",
+  messagingSenderId: "950148001099",
+  appId: "1:950148001099:web:f084a420ca599e68b54cce"
+};
+
+const app = initializeApp(firebaseConfig);
