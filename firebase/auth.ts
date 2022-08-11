41865530cdb492073d412d358b36a2f1be1c06e8
kingvyas <kumarvyasss@gmail.com>
2022-08-11 20:18:40 +0530

    adding firebase auth

---
+++ firebase/auth.ts
@@ -0,0 +1,16 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+import app from "./config";
+
+const auth = getAuth(app)
+
+const googleProvider = new GoogleAuthProvider()
+
+const signInWithGoogle = async ()=>{
+    try {
+        
+    } catch (err) {
+        
+    }
+    signInWithPopup
+}
