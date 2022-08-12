9e60c016ae056f1d0625bc3daffdee0d67a00045
kingvyas <kumarvyasss@gmail.com>
2022-08-12 15:14:56 +0530

    added firebase add user

--- firebase/db.ts
+++ firebase/db.ts
@@ -1,7 +1,19 @@
-import { getFirestore } from "firebase/firestore";
-
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 import app from "./config";
 
+// Firestore database reference
 const db = getFirestore(app);
+
+// Users collection
+const users = collection(db, "users");
+
+// Add user function
+const addUser = async (data) => {
+  try {
+    await addDoc(users, data);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export { db };
